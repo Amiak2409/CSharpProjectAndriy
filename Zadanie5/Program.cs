41a41b79ca53d8444df390b72bbf2884b7267fdf
Andriy Yanchik <149189079+Amiak2409@users.noreply.github.com>
2024-02-03 14:18:20 +0100

    Update Program.cs

--- Zadanie5/Program.cs
+++ Zadanie5/Program.cs
@@ -1,19 +1,31 @@
-﻿
+
+
+//Account account1 = new Account(10294,50.5f);
+//account1.Deposit(100.5f);
+//account1.Withdraw(10.5f);
+//Account account2 = new Account(10638, 1000.5f);
+//account2.Deposit(40.0f);
+//account2.Withdraw(0.5f);
+
+Transaction transaction = new Transaction();
+
+//transaction.makeTransaction(account2, account1, 500.0f);
+//Console.WriteLine(account1.GetBalance);
+//Console.WriteLine(account2.GetBalance);
+
+SavingsAccount savingsAccount = new SavingsAccount(10365,100.0f);
+savingsAccount.Deposit(40);
+savingsAccount.Withdraw(40);
+CheckingAccount checkingAccount = new CheckingAccount(10324,150.0f);
+checkingAccount.Deposit(90);
+checkingAccount.Withdraw(40);
+InvestmentAccount investmentAccount = new InvestmentAccount(10289,500.0f);
+investmentAccount.Deposit(100);
+investmentAccount.Withdraw(10);
+investmentAccount.Invest(400,"Apple");
+
+
 BankManager bankManager = new BankManager();
-
-SavingsAccount savingsAccount = (SavingsAccount)bankManager.OpenAccount("Savings", 1000);
-InvestmentAccount investmentAccount = (InvestmentAccount)bankManager.OpenAccount("Investment", 500);
-
-savingsAccount.Deposit(500); 
-investmentAccount.Deposit(200); 
-
-bankManager.MakeTransaction(savingsAccount, investmentAccount, 300);
-
-float savingsBalance = savingsAccount.GetBalance;
-float CheckBalance = investmentAccount.GetBalance;
-Console.WriteLine(savingsBalance);
-Console.WriteLine(CheckBalance);
-
 
 
 class Transaction : ITransaction
@@ -46,12 +58,12 @@ class Account : IComparable<Account>
 
     public int GetAccNumber { get { return _accountNumber; } set { _accountNumber = value; } }
     public float GetBalance { get { return _balance; } set { _balance = value; } }
-    public void Deposit(float amount)
+    public virtual void Deposit(float amount)
     {
         if (amount > 0)
         {
             _balance += amount;
-            Console.WriteLine($"Great! Your deposi: {amount}$ hes been confirmed! Now your balance is {_balance}$");
+            Console.WriteLine($"Great! Your deposit: {amount}$ hes been confirmed! Now your balance is {_balance}$");
         }
         else
         {
@@ -91,6 +103,8 @@ class Account : IComparable<Account>
     {
         Console.WriteLine($"{_balance}$");
     }
+
+    
 }
 
 interface ITransaction 
@@ -102,51 +116,95 @@ interface ITransaction
 
 class SavingsAccount : Account
 {
-    private float _interestRate { get;set; }
-
-    public SavingsAccount ( int accountNumber, float balance)
-        :base(accountNumber, balance)
+    private float commission = 5;
+    private int _accountNumber = 0;
+    private float _balance = 0.0f;
+    
+    public SavingsAccount(int accountNumber, float balance) : 
+        base(accountNumber, balance)
     {
-        _interestRate = balance;
+        _accountNumber = accountNumber;
+        _balance = balance;
     }
-    public void CalculateInterest()
+    public void GetAccount()
     {
-        float interest = GetBalance * _interestRate;
-        GetBalance += interest;
-        Console.WriteLine($"Interest calculate.Now Your Balnce {GetBalance}");
+        Console.WriteLine($"Account number is: {_accountNumber} \nBalance is: {_balance}");
+    }
+    void Deposit(float amount)
+    {
+        if (amount > 0)
+        {
+            _balance += amount;
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+    public override void Withdraw(float amount)
+    {
+        float amountCommission = amount * (commission / 100);
+        amount += amountCommission;
+        if(amount > 0 && amount <= _balance)
+        {
+            _balance -= amount;
+        }
+        Console.WriteLine($"Great! Your withdraw: {amount - amountCommission}$ hes been confirmed! Now your balance is {_balance}$ commision is: {amountCommission}$");
+
     }
 }
 
 class CheckingAccount : Account
 {
-    public float _overdraftLimit { get; set; }
-    public CheckingAccount(int accountNumber, float balance)
-        : base(accountNumber, balance)
-    {
+    public float _balance = 0.0f;
+    private int _accountNumber = 0;
+    private float commission = 1;
 
-    }
-    public void Withdraw(float amount, float overdraftLimit)
+    public CheckingAccount( int accountNumber,float balance):
+        base(accountNumber,balance)
     {
-        _overdraftLimit = overdraftLimit;
-        if (amount <= _overdraftLimit + GetBalance)
+        _balance = balance;
+        _accountNumber = accountNumber;
+    }   
+    public void Deposit(int amount)
+    {
+        if (amount > 0)
         {
-            GetBalance -= amount;
-            Console.WriteLine($"Great! Your withdraw: {amount}$ hes been confirmed! Now your balance is {GetBalance}$");
+            _balance += amount;
+            Console.WriteLine($"Great! Your deposit: {amount}$ hes been confirmed! Now your balance is {_balance}$");
         }
         else
         {
-            Console.WriteLine($"Error: 0x023c45 (OverdraftLimit is: {_overdraftLimit}$)");
+            Console.WriteLine("Error: 0x002c51 (Summ of deposit must be greaten then 0)");
         }
     }
+    public override void Withdraw(float amount)
+    {
+        float amountCommission = amount * (commission / 100);
+        amount += amountCommission;
+        if (amount > 0 && amount <= _balance)
+        {
+            _balance -= amount;
+        }
+        Console.WriteLine($"Great! Your withdraw: {amount - amountCommission}$ hes been confirmed! Now your balance is {_balance}$ commision is: {amountCommission}$");
+
+    }
+
+
 }
 
 class InvestmentAccount : Account
 {
     public string _investmentType { get; set; }
+    private float _balance = 0.0f;
+    private int _accountNumber = 0;
+    private float commission = 5;
 
     public InvestmentAccount(int accountNumber, float balance)
         :base(accountNumber, balance)
     {
+        _balance = balance;
+        _accountNumber = accountNumber;
     }
 
     public void Invest(float amount, string InvestmentType)
@@ -164,11 +222,35 @@ class InvestmentAccount : Account
 
         }
     }
+    public void Deposit(int amount)
+    {
+        if (amount > 0)
+        {
+            _balance += amount;
+            Console.WriteLine($"Great! Your deposit: {amount}$ hes been confirmed! Now your balance is {_balance}$");
+        }
+        else
+        {
+            Console.WriteLine("Error: 0x002c51 (Summ of deposit must be greaten then 0)");
+        }
+    }
+    public override void Withdraw(float amount)
+    {
+        float amountCommission = amount * (commission / 100);
+        amount += amountCommission;
+        if (amount > 0 && amount <= _balance)
+        {
+            _balance -= amount;
+        }
+        Console.WriteLine($"Great! Your withdraw: {amount - amountCommission}$ hes been confirmed! Now your balance is {_balance}$ commision is: {amountCommission}$");
+
+    }
 }
 
 class BankManager
 {
     private int accountNumber = 1;
+    List<Account> accounts = new List<Account>();
     public Account _account;
     public Account OpenAccount(string AccountType,float initialBalance)
     {
@@ -176,23 +258,22 @@ class BankManager
         {
             SavingsAccount account = new SavingsAccount(accountNumber, initialBalance);
             _account = account;
+            accounts.Add(account);
             accountNumber++;
-
         }
         else if (AccountType.ToUpper() == "checking")
         {
             CheckingAccount account = new CheckingAccount(accountNumber, initialBalance);
             _account = account;
             accountNumber++;
-
-
+            accounts.Add(account);
         }
         else if( AccountType.ToLower() == "investment")
         {
             InvestmentAccount account = new InvestmentAccount(accountNumber, initialBalance);
             _account = account;
             accountNumber++;
-
+            accounts.Add(account);
         }
         else
         {
@@ -200,6 +281,11 @@ class BankManager
         }
         return _account;
     }
+    public void CloseAccount(int accountNumber)
+    {
+        accounts.Remove(accounts[accountNumber]);
+    }
+    
 
     public float CheckAccount(Account account)
     {
@@ -212,7 +298,7 @@ class BankManager
         {
             sourceAccount.GetBalance -= amount;
             targetAccoun.GetBalance += amount;
-            Console.WriteLine("Transaction Finishet");
+            Console.WriteLine("Transaction Finished");
         }
         else
         {
