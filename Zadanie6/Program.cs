6358a58a493fab152ea9a007ae2eb9bb30195b38
Andriy Yanchik <149189079+Amiak2409@users.noreply.github.com>
2024-02-11 11:43:04 +0100

    Add files via upload

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,50 @@
+﻿
+public class CustomStack<T>
+{
+    public T[] stack;
+    public int index = 0;
+    public int size = 0;
+    public int count = 0;
+    public CustomStack(int size)
+    {
+        stack = new T[size];
+        index = size;
+        this.size = size;
+    }
+    public void Push(T item)
+    {
+        stack[index - 1] = item;
+        index--;
+        count++;
+    }
+    public void Pop()
+    {
+        int index1 = index;
+        index--;
+        stack[index1] = default(T);
+        count--;
+    }
+    public T Peek()
+    {
+        return stack[index];
+    }
+    public void Clear()
+    {
+        for (int i = 0;i < stack.Length; i++)
+        {
+            stack[i] = default(T);
+        }
+    }
+    public bool isEmpty()
+    {
+        if (count == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    public int Count() 
+    {
+        return count;
+    }
+}
